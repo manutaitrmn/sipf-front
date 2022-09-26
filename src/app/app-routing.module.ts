0d3b254b716637d3f17ff307e9800196394a16f1
Manutai Teraiamano <manutai@teraiamano.com>
2022-09-26 19:05:18 +0200

    remove consolelogs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouteGuardService } from './services/route-guard.service';
+import {MessagesComponent} from "./messages/messages.component";
+import {MessageListComponent} from "./messages/message-list/message-list.component";
+import {BaseComponent} from "./base/base.component";
+import {MessageNewComponent} from "./messages/message-new/message-new.component";
+import {MessageEditComponent} from "./messages/message-edit/message-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    component: BaseComponent,
+    canActivate: [RouteGuardService],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'messages',
+        component: MessagesComponent,
+        children: [
+          { path: '', component: MessageListComponent },
+          { path: 'new', component: MessageNewComponent },
+          { path: ':id/edit', component: MessageEditComponent },
+        ]
+      },
+    ]
+  },
+
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { MessagesComponent } from './messages/messages.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
+import { BaseComponent } from './base/base.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import { MessageNewComponent } from './messages/message-new/message-new.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import {MatSelectModule} from "@angular/material/select";
+import localeFr from "@angular/common/locales/fr";
+import {registerLocaleData} from "@angular/common";
+import { MessageBannerComponent } from './messages/message-banner/message-banner.component';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorComponent,
+    HomeComponent,
+    MessagesComponent,
+    HeaderComponent,
+    MessageListComponent,
+    BaseComponent,
+    MessageNewComponent,
+    MessageEditComponent,
+    MessageBannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
